01d586637c0ea3578570c9380557874ce09877f8
Matthew Ursaki <ursaki@uleth.ca>
2023-06-14 09:19:19 -0600

    Latest before redo (don't click that decimal!)

--- Simple Calculator/MainWindow.xaml.cs
+++ Simple Calculator/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace Simple_Calculator
     {
         double inp1, inp2, resultnum = 0;
         bool isFirst = true;
-        bool equalsClicked = false;
+        bool isDecimal = false;
         string currExpr = "";
         public enum Operation
         {   
@@ -50,8 +50,9 @@ namespace Simple_Calculator
 
             if (isFirst)
             {
-                expression.Content = Expression_Updater(currExpr, content.ToString());
+                
                 contentVal = Number_Handler(inp1, contentVal);
+                expression.Content = Expression_Updater(currExpr, content.ToString());
                 inp1 = contentVal;
             }
             else
@@ -74,33 +75,29 @@ namespace Simple_Calculator
             {
                 case "+":
                     op = Operation.Addition;
-                    isFirst = false;
                     expression.Content = Expression_Updater(currExpr, " + ");
                     break;
                 case "-":  
                     op = Operation.Subtraction;
-                    isFirst = false;
                     expression.Content = Expression_Updater(currExpr, " - ");
                     break;
                 case "*":
                     op = Operation.Multiplication;
-                    isFirst = false;
                     expression.Content = Expression_Updater(currExpr, " x ");
                     break;
                 case "/":
                     op = Operation.Division;
-                    isFirst = false;
                     expression.Content = Expression_Updater(currExpr, " / ");
                     break;
                 case "Modulo":
                     op = Operation.Modulo; 
-                    isFirst = false;
                     expression.Content = Expression_Updater(currExpr, " mod ");
                     break;
                 default:
                     op = Operation.None;
                     break;
             }
+            isFirst = false;
         }
 
         private void EqualsButton_Click(object sender, RoutedEventArgs e)
@@ -111,45 +108,54 @@ namespace Simple_Calculator
             {
                 case Operation.Addition:
                     resultnum = inp1 + inp2;
-                    inp1 = resultnum;
-                    inp2 = 0;
-                    isFirst = true;
+
                     break;
                 case Operation.Subtraction:
                     resultnum = inp1 - inp2;
-                    inp1 = resultnum;
-                    inp2 = 0;
-                    isFirst = true;
+
                     break;
                 case Operation.Multiplication:
                     resultnum = inp1 * inp2;
-                    inp1 = resultnum;
-                    inp2 = 0;
-                    isFirst = true;
+
                     break;
                 case Operation.Division:   
                     resultnum = inp1 / inp2;
-                    inp1 = resultnum;
-                    inp2 = 0;
-                    isFirst = true;
+
                     break;
                 case Operation.Modulo:
                     resultnum = inp1 % inp2;
-                    inp1 = resultnum;
-                    inp2 = 0;
-                    isFirst = true;
+
                     break;
             }
+
+            inp1 = resultnum;
+            inp2 = 0;
+            isFirst = true;
+
             expression.Content = currExpr + " = ";
             currExpr = resultnum.ToString();
             result.Content = resultnum;
+        }
 
+        private void DecimalButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            isDecimal = true;
         }
 
         private double Number_Handler(double currentInp, double newInp)
         {
-            return (currentInp * 10 + newInp);
+            if (!isDecimal)
+            {
+                return (currentInp * 10 + newInp);
+            }
+            else
+            {
+                return (currentInp + 0.1);
+            }
         }
+
 
         private string Expression_Updater(string curr, string entry)
         {
